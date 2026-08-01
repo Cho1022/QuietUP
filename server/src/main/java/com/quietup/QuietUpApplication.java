4955d358761bf9dfe3a7e26e99b4ac2294cdc873
Beom Sang <cbs001022@gmail.com>
2026-08-02 03:25:02 +0900

    chore(server): 스프링 부트 서버 실행 기반 구성

---
+++ server/src/main/java/com/quietup/QuietUpApplication.java
@@ -0,0 +1,13 @@
+package com.quietup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuietUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuietUpApplication.class, args);
+	}
+
+}
